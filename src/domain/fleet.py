d999623b1b7daf4694f6efd6a594caa20eea41c1
Gustavo Cazzine <cazzinengustavo@gmail.com>
2026-04-23 20:01:24 -0300

    feat: cria o FleetManager para orquestrar frota e centralizar analise de telemetria

---
+++ src/domain/fleet.py
@@ -0,0 +1,38 @@
+from domain.analyzer import RiskAnalyzer
+
+
+class FleetManager:
+    def __init__(self):
+        self._equipments = {}
+        self._analyzer = RiskAnalyzer()
+
+    def register_equipment(self, equipment):
+        if equipment.equipment_id in self._equipments:
+            return False
+        
+        self._equipments[equipment.equipment_id] = equipment
+        return True
+    
+    def process_telemetry(self, telemetry_read):
+        id_equipment = telemetry_read.equipment_id
+        if not id_equipment:
+            return False
+        
+        if id_equipment not in self._equipments:
+            return False
+        
+        equipment = self._equipments[id_equipment]
+        self._analyzer.evaluate_telemetry(equipment, telemetry_read)
+
+    def get_fleet_status(self):
+        cont_operacional, cont_alerta, cont_manutencao = 0
+        for equipment in self._equipments.values():
+            match equipment.status:
+                case "OPERACIONAL":
+                    cont_operacional +=1
+                case "ALERTA":
+                    cont_alerta += 1
+                case "MANUTENCAO":
+                    cont_manutencao +=1
+
+        return f"Equipamentos em Operacional: {cont_operacional}\nEquipamentos em Alerta: {cont_alerta}\nEquipamentos em Manutenção: {cont_manutencao}"
